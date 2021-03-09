1df715d5976efb94736662b6ebc71eb5cb00f921
teunstout <teunstout@hotmail.com>
2021-03-09 12:29:03 +0100

    Setup project - fontend - typescript - scss

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from "vue-class-component";
+
+@Options({
+  components: {},
+})
+export default class App extends Vue {
+}
+</script>
+
+<style lang="scss"></style>
