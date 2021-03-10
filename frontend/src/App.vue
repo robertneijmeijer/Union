1c3db09537687d87976d09fc1f5035c0309d9407
joel <joel_verhagen_1997@hotmail.com>
2021-03-10 11:27:09 +0100

    Register screen
    + architecture with components and views

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,35 @@
 <template>
-    <div id="app">
-        <login></login>
-    </div>
+  <div id="app">
+    <register-view></register-view>
+  </div>
 </template>
 
 <script lang="ts">
-    import {Options, Vue} from "vue-class-component";
-    import login from './components/login.vue';
+import { Options, Vue } from "vue-class-component";
+import loginView from "./views/loginView.vue";
+import registerView from "./views/registerView.vue";
 
-    @Options({
-        components: {login},
-    })
-    export default class App extends Vue {
-    }
+@Options({
+  components: { loginView: loginView, registerView: registerView },
+})
+export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-    #app {
-        width: 100%;
-        height: 100%;
-    }
+@import "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
+@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
 
-    html, body {
-        background-image: url('https://marmotamaps.com/de/fx/wallpaper/download/alpenberge/Marmotamaps_Wallpaper_MonteViso_Desktop_1920x1080.jpg');
-        background-size: cover;
-        background-repeat: no-repeat;
-        height: 100%;
-        font-family: 'Numans', sans-serif;
-    }
+#app {
+  width: 100%;
+  height: 100%;
+}
+
+html,
+body {
+  background-image: url("https://marmotamaps.com/de/fx/wallpaper/download/alpenberge/Marmotamaps_Wallpaper_MonteViso_Desktop_1920x1080.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100%;
+  font-family: "Numans", sans-serif;
+}
 </style>
