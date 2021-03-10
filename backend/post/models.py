ef79bd0daac7117813673e1f7bd63bff2c958c3b
Robert Neijmeijer <Robert.Neijmeijer@hva.nl>
2021-03-10 11:30:39 +0100

    Added post model

---
+++ backend/post/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_lenght=300)
+    message = models.CharField(max_lenght=50000)
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # TODO: uncomment when the below models are made
+    # user_id = models.ForeignKey(
+    #     'user',
+    #     on_delete=models.SET_NULL,
+    #     null=True
+    # )
+    #
+    # union_id = models.ForeignKey(
+    #     'union',
+    #     on_delete=models.PROTECTED,
+    #     null=False
+    # )
