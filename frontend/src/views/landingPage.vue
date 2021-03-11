f8f4152df8520b0211d9a628ff7739f9e8f001c1
Jverhagen11 <70578766+Jverhagen11@users.noreply.github.com>
2021-03-11 13:01:54 +0100

    Feature/un 24 landingspage (#9)
    * landingspage done
    
    * fixed theme
    
    * edit
    
    * forgot import
    
    * changed images on landingspage
    
    Co-authored-by: joel <joel_verhagen_1997@hotmail.com>

---
+++ frontend/src/views/landingPage.vue
@@ -0,0 +1,61 @@
+<template>
+    <navigator></navigator>
+    <div class="landingsimage">
+    <img src="../assets/landingspage.jpg" height="583" width="824"/>
+    <div class="centeredText">
+        <h3 class="first">Create.</h3>
+        <h3 class="second">Discover.</h3>
+        <h3 class="third">Unite.</h3>
+    </div>
+    </div>
+</template>
+
+<script>
+  import Navigator from "@/components/navigator";
+
+  export default {
+    name: "landingPage",
+    components: { Navigator },
+  };
+</script>
+
+<style lang="scss" scoped>
+    @import "../assets/theme";
+
+    .centeredText {
+        text-align: center;
+        position: absolute;
+        top: 40px;
+        left: 45%;
+    }
+
+    .first {
+        margin-top: 200px;
+        color: white;
+        user-select: none;
+    }
+
+    .second {
+        color: white;
+        margin-top: 10px;
+        user-select: none;
+    }
+
+    .third {
+        color: $unionBlue;
+        margin-top: 10px;
+        user-select: none;
+    }
+
+    .landingsimage{
+       position: relative;
+        width: 100%;
+    }
+
+    img{
+        margin-top: 50px;
+    }
+
+
+
+</style>
