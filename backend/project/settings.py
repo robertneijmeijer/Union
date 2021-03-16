c70a8e6ce01ef86fefbdec387a6f3aa184c86d0c
Teun Stout <60847859+teunstout@users.noreply.github.com>
2021-03-16 12:09:09 +0100

    Docker compose create containers for postgres and django

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
         'HOST': env("DATABASE_HOST"),
