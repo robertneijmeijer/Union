e1fe8d948a3d2fbead56db4873f483519ffd4b30
Teun Stout <60847859+teunstout@users.noreply.github.com>
2021-03-16 12:30:43 +0100

    Done with db and postgres

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -84,14 +84,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
-        'PORT': env("DATABASE_PORT"),
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "root",
+        'HOST': "database",
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
