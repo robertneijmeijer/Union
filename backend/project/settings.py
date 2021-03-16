a863609c9d3dec1269fc5d0f0a8914e94636159d
Teun Stout <60847859+teunstout@users.noreply.github.com>
2021-03-16 13:37:35 +0100

    Remove env

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -11,13 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
-
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
