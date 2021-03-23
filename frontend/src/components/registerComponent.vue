df000d4d0903653c30b5aa9a075d6ca866735acd
Jverhagen11 <70578766+Jverhagen11@users.noreply.github.com>
2021-03-23 19:43:14 +0100

    Feature/un 13 frontend register styling#2 (#22)
    * register styling
    
    * styling for register
    
    * fixed formatting
    
    Co-authored-by: joel <joel_verhagen_1997@hotmail.com>

--- frontend/src/components/registerComponent.vue
+++ frontend/src/components/registerComponent.vue
@@ -1,152 +1,163 @@
 <template>
-  <div class="container">
-    <div class="d-flex justify-content-center h-100">
-      <div class="card">
-        <div class="card-header">
-          <h3 style="color: #ffffff; ">Sign Up</h3>
+    <div class="container">
+        <div class="d-flex justify-content-center h-100">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="registerTitle">Sign up to Union</h5>
+                </div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fa fa-user fa-lg center black"></i>
+                                </span>
+                            </div>
+                            <input type="text" name="uid" class="form-control input" placeholder="Enter Username" />
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text white"><i class="fa fa-envelope fa-lg unique"></i></span>
+                            </div>
+                            <input type="text" name="mail" class="form-control input" placeholder="Enter Email" />
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text black"><i class="fa fa-lock fa-lg center"></i></span>
+                            </div>
+                            <input type="password" name="pwd" class="form-control input" placeholder="Enter Password" />
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text black"><i class="fa fa-lock fa-lg center"></i></span>
+                            </div>
+                            <input type="password" name="pwd-repeat" class="form-control input" placeholder="Repeat Password" />
+                        </div>
+                        <div class="form-group">
+                            <div class="centerButton">
+                                <button class="btn btn-primary register_btn" type="submit" name="login-button">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-center links">Already a member?<a class="linkText" v-on:click="toLogin" href="">Sign In</a></div>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-          <form action="" method="post">
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-                <span style="color: white" class="input-group-text"
-                  ><i class="fa fa-user fa-lg center"></i
-                ></span>
-              </div>
-              <input
-                type="text"
-                name="uid"
-                class="form-control input"
-                placeholder="Enter Username"
-              />
-            </div>
-
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-                <span style="color: white" class="input-group-text"
-                  ><i
-                    style="margin-left: 3px"
-                    class="fa fa-envelope fa-lg center"
-                  ></i
-                ></span>
-              </div>
-              <input
-                type="text"
-                name="mail"
-                class="form-control input"
-                placeholder="Enter Email"
-              />
-            </div>
-
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-                <span style="color: white" class="input-group-text"
-                  ><i class="fa fa-lock fa-lg center"></i
-                ></span>
-              </div>
-              <input
-                type="password"
-                name="pwd"
-                class="form-control input"
-                placeholder="Enter Password"
-              />
-            </div>
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-                <span style="color: white" class="input-group-text"
-                  ><i class="fa fa-lock fa-lg center"></i
-                ></span>
-              </div>
-              <input
-                type="password"
-                name="pwd-repeat"
-                class="form-control input"
-                placeholder="Repeat Password"
-              />
-            </div>
-
-            <div class="form-group">
-              <button
-                class="btn btn-primary float-right"
-                type="submit"
-                name="login-button"
-              >
-                Register
-              </button>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <div class="d-flex justify-content-center links">
-            Already a member?<a href="">Sign In</a>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
-
 <script>
-export default {
-  name: "registerComponent",
-};
+  import router from "@/router";
+
+  export default {
+    name: "registerComponent",
+    methods: {
+      toLogin: function () {
+        router.push("login");
+      },
+    },
+  };
 </script>
-
 <style lang="scss" scoped>
-.container {
-  height: 100%;
-  align-content: center;
-}
+    @import "../assets/theme";
 
-.card {
-  height: 400px;
-  margin-top: auto;
-  margin-bottom: auto;
-  width: 400px;
-  background-color: rgba(0, 0, 0, 0.5) !important;
-}
+    .container {
+        height: 100%;
+        align-content: center;
+    }
 
-.card-header h3 {
-  color: white;
-}
+    .black {
+        color: black;
+    }
 
-.social_icon {
-  position: absolute;
-  right: 20px;
-  top: -45px;
-}
+    .white {
+        color: white;
+    }
 
-.input-group-prepend span {
-  width: 50px;
-  background-color: #017bff;
-  color: black;
-  border: 0 !important;
-}
+    .unique {
+        margin-left: 3px;
+        color: black;
+    }
 
-input:focus {
-  outline: 0 0 0 0 !important;
-  box-shadow: 0 0 0 0 !important;
-}
+    .card {
+        height: 410px;
+        margin-top: auto;
+        margin-bottom: auto;
+        width: 400px;
+        background-color: $buttonHoverColor;
+    }
 
-.remember {
-  color: white;
-}
+    .registerTitle {
+        color: white;
+        text-align: center;
+        margin-top: 10px;
+        user-select: none;
+    }
 
-.remember input {
-  width: 20px;
-  height: 20px;
-  margin-left: 15px;
-  margin-right: 5px;
-}
+    .card-header h3 {
+        color: white;
+    }
 
-.login_btn {
-  color: white;
-  background-color: #017bff;
-  width: 100px;
-  border: 2px solid #017bff;
-}
+    .social_icon {
+        position: absolute;
+        right: 20px;
+        top: -45px;
+    }
 
-.links a {
-  margin-left: 4px;
-}
+    .input-group-prepend span {
+        width: 50px;
+        background-color: $unionBlue;
+        color: black;
+        border: 0 !important;
+    }
+
+    input:focus {
+        outline: 0 0 0 0 !important;
+        box-shadow: 0 0 0 0 !important;
+    }
+
+    .remember {
+        color: white;
+    }
+
+    .remember input {
+        width: 20px;
+        height: 20px;
+        margin-left: 15px;
+        margin-right: 5px;
+    }
+
+    .centerButton {
+        width: 100%;
+        text-align: center;
+    }
+
+    .register_btn {
+        color: white;
+        width: $buttonwidth;
+        border-radius: $borderRadius;
+        border: $buttonBorder $unionBlue;
+        background: transparent;
+        margin-top: 10px;
+
+        &:hover {
+            background-color: $buttonHoverColor;
+            border: $buttonBorder $buttonHoverColor;
+        }
+    }
+
+    .links a {
+        margin-left: 4px;
+    }
+
+    .linkText {
+        color: white;
+    }
+
+    .center {
+        margin-left: 6px;
+    }
 </style>
