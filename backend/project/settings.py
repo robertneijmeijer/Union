1eef3c467426149e6bfc26af28f82b6b0f9652ad
Yan Alexandre <yan.lanna.alexandre@hva.nl>
2021-03-23 10:32:32 +0100

    Exception handling is on point

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -36,6 +36,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'EXCEPTION_HANDLER': 'core.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error',
 }
 
 AUTH_USER_MODEL = 'users.User'
@@ -126,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
