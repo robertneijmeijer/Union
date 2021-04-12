fd2641898ca49d4affca5085a2807388d85fec08
teunstout <teunstout@hotmail.com>
2021-04-12 13:29:20 +0200

    Pagination

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,9 +1,17 @@
-from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
 from post.models import Post
 from post.serializer import PostSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostsPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    pagination_class = PostsPagination
