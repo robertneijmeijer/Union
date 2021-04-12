80718c56b7c84f0ee2601584063edb308dacba52
Koen Lippe <koen.lippe@quicknet.nl>
2021-04-12 13:07:03 +0200

    60 - Updated standard functions

--- frontend/src/api/api-base.ts
+++ frontend/src/api/api-base.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosResponse} from "axios";
+import axios, {AxiosRequestConfig, AxiosResponse} from "axios";
 
 export default class ApiBase {
     private static BASE_URL = "http://localhost:8000" // TODO: Replace with env var
@@ -6,31 +6,49 @@ export default class ApiBase {
     protected static executeRequest(
         method: "get" | "delete" | "post" | "put",
         url: string,
+        config: AxiosRequestConfig
     ): Promise<AxiosResponse> {
         // TODO Retrieve token
         const sampleToken = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6OCwiZXhwIjoxNjIzNDA3MDQ0fQ.CWsq5ireKWE5lFAlorLmAFGtP0pf17nIrE_5b2VU2v4\''
+        const headers = {
+            Authorization: `Bearer ${sampleToken}`
+        }
+
         return axios
             .request({
                 method,
                 url,
-                headers: { Authorization: `Bearer ${sampleToken}`}
+                headers,
+                ...config,
             })
             .then(response => response)
             .catch(handleError)
+
+        // TODO: Test if auth headers is overridden if config variable is applied in functions below
     }
 
-    public static async requestGet(resource: string, id: String) {
-        return this.executeRequest("get", `${ApiBase.BASE_URL}/${resource}/${id}/`)
+    public static async requestGet(resource: string, id: string, config?: AxiosRequestConfig) {
+        return this.executeRequest(
+            "get", `${ApiBase.BASE_URL}/${resource}/${id}/`, {...config}
+        )
     }
 
-    public static requestGetAll(resource: string) {
-        return this.executeRequest("get", `${ApiBase.BASE_URL}/${resource}/`)
+    public static async requestGetAll(resource: string, config?: AxiosRequestConfig) {
+        return this.executeRequest(
+            "get", `${ApiBase.BASE_URL}/${resource}/`, {...config}
+        )
+    }
+
+    public static async requestPost(resource: string, config?: AxiosRequestConfig) {
+        return this.executeRequest(
+            "post", `${ApiBase.BASE_URL}/${resource}`, {...config}
+        )
     }
 }
 
 
 const handleError = (error: any) => {
-    if(error.data) {
+    if (error.data) {
         return error.data
     }
 
--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -2,15 +2,9 @@ import ApiBase from "@/api/api-base";
 
 export default class UserApi extends ApiBase {
 
-    public static getUsers = () => {
-        ApiBase.requestGetAll("users")
-            .then(response => {
-                console.log(response)
-                return response
-            })
-            .catch(error => {
-                console.log(error)
-                return error
-            })
+    public static getUsers = (): Promise<any> => {
+        return ApiBase.requestGetAll("users")
+            .then(response => response)
+            .catch(error => error)
     }
 }
