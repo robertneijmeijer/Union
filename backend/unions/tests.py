b46f823b28d49589ce84f28bc4d2898e3e82a80f
teunstout <teunstout@hotmail.com>
2021-04-12 12:00:04 +0200

    Update tests

--- backend/unions/tests.py
+++ backend/unions/tests.py
@@ -2,7 +2,7 @@ from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 
 from users.models import User
-from union.models import Union
+from unions.models import Union
 import json
 import logging
 
@@ -17,38 +17,36 @@ class CreateUnion(APITestCase):
         union: Union = Union(name="crypto", description="", members_can_invite=True, creator=self.teun)
         union.save()
 
-        req = self.client.get('/union/', format='json')
-    #
-    #     self.assertEqual(req.status_code, status.HTTP_200_OK)
-    #
-    # content_unicode = req.content.decode('utf-8')
-    # content = json.loads(content_unicode)
+        req = self.client.get('/unions/', format='json')
 
-    # self.assertTrue(len(content) > 0)
-    # self.assertEqual(content[0]['name'], "crypto")
-    # self.assertEqual(content[0]['creator_id'], self.teun.user_id)
+        self.assertEqual(req.status_code, status.HTTP_200_OK)
 
-    # def test_create_union_endpoint(self):
-    #     union_data = {
-    #         "name": "test",
-    #         "description": "test",
-    #         "members_can_invite": True,
-    #         "creator_id": self.teun.user_id,
-    #         "union_users": []
-    #     }
-    #     req = self.client.post('/union/', union_data, format='json')
-    #     self.assertEqual(req.status_code, status.HTTP_201_CREATED)
-    #
-    # def test_update_union_endpoint(self):
-    #     union: Union = Union(name="test", description="name should be 'Worked'", members_can_invite=True,
-    #                          creator=self.teun)
-    #     union.save()
-    #     req = self.client.patch(f'/union/{union.union_id}/', {"name": "Worked"}, format='json')
-    #     self.assertEqual(req.status_code, status.HTTP_200_OK)
-    #
-    # def test_delete_union_endpoint(self):
-    #     union: Union = Union(name="test", description="name should be 'Worked'", members_can_invite=True,
-    #                          creator=self.teun)
-    #     union.save()
-    #     req = self.client.delete(f'/union/{union.union_id}/', format='json')
-    #     self.assertEqual(req.status_code, status.HTTP_204_NO_CONTENT)
+        content_unicode = req.content.decode('utf-8')
+        content = json.loads(content_unicode)
+
+        self.assertTrue(len(content) > 0)
+        self.assertEqual(content[0]['name'], "crypto")
+        self.assertEqual(content[0]['creator_id'], self.teun.user_id)
+
+    def test_create_union_endpoint(self):
+        union_data = {
+            "name": "test",
+            "members_can_invite": True,
+            "creator_id": self.teun.user_id,
+        }
+        req = self.client.post('/unions/', union_data, format='json')
+        self.assertEqual(req.status_code, status.HTTP_201_CREATED)
+
+    def test_update_union_endpoint(self):
+        union: Union = Union(name="test", description="name should be 'Worked'", members_can_invite=True,
+                             creator=self.teun)
+        union.save()
+        req = self.client.patch(f'/unions/{union.union_id}/', {"name": "Worked"}, format='json')
+        self.assertEqual(req.status_code, status.HTTP_200_OK)
+
+    def test_delete_union_endpoint(self):
+        union: Union = Union(name="test", description="name should be 'Worked'", members_can_invite=True,
+                             creator=self.teun)
+        union.save()
+        req = self.client.delete(f'/unions/{union.union_id}/', format='json')
+        self.assertEqual(req.status_code, status.HTTP_204_NO_CONTENT)
