6f1f254315dd0b22f0acfa88bcd4ce89f3df4990
Koen Lippe <koen.lippe@quicknet.nl>
2021-04-14 09:09:24 +0200

    60 - Fixed slashed on frontend

--- frontend/src/api/api-base.ts
+++ frontend/src/api/api-base.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosRequestConfig, AxiosResponse} from "axios";
 
 export default class ApiBase {
-    private static BASE_URL = "http://localhost:8000" // TODO: Replace with env var
+    private static BASE_URL = "http://127.0.0.1:8000" // TODO: Replace with env var
 
     protected static executeRequest(
         method: "get" | "delete" | "post" | "put",
@@ -21,19 +21,19 @@ export default class ApiBase {
 
     public static async requestGet(resource: string, id: string, config?: AxiosRequestConfig) {
         return this.executeRequest(
-            "get", `/${resource}/${id}/`, {...config}
+            "get", `/${resource}/${id}`, {...config}
         )
     }
 
     public static async requestGetAll(resource: string, config?: AxiosRequestConfig) {
         return this.executeRequest(
-            "get", `/${resource}/`, {...config}
+            "get", `/${resource}`, {...config}
         )
     }
 
     public static async requestPost(resource: string, config?: AxiosRequestConfig) {
         return this.executeRequest(
-            "post", `/${resource}/`, {...config}
+            "post", `/${resource}`, {...config}
         )
     }
 }
