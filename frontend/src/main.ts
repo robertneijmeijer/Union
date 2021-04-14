cdde3b336fe8a225dd3d03c7cb3a349b8e55db5c
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2021-04-14 09:48:55 +0200

    Removed dead code

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,12 +2,9 @@ import {createApp} from "vue";
 import App from "./App.vue";
 import {store} from "@/store/store";
 import router from "@/router";
-import i18n from "./i18n"
 
 const app = createApp(App);
 
 app.use(store);
-
-app.use(i18n);
 
 app.use(router).mount("#app");
