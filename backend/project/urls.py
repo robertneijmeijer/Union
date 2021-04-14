71958acb30d5d6cb3986205c6ecc82ed79b1c3fd
Koen Lippe <koen.lippe@quicknet.nl>
2021-04-14 10:02:10 +0200

    Feature/un 60 central api file (#31)
    * 60 - Initial functionality
    
    * 60 - Updated standard functions
    
    * 60 - Added Slash to API routes and allow credentials
    
    * 60 - Fixed user routing
    
    * 60 - Fixed slashed on frontend
    
    * 60 - Removed comment and reference

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -3,16 +3,14 @@ from django.urls import path, include
 from rest_framework import routers
 
 from post.views import PostViewSet
-from users.views import UserViewSet
-from unions.views import UnionViewSet
+from union.views import UnionViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 router.register(r'post', PostViewSet)
 router.register(r'unions', UnionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('users/', include('users.urls')),
+    path('users', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,10 +4,10 @@ from django.urls import include, path
 from users.views import UserViewSet, RegistrationAPIView, LoginAPIView
 
 userRouter = routers.DefaultRouter()
-userRouter.register(r'', UserViewSet, basename='user')
+userRouter.register(r'', UserViewSet)
 
 urlpatterns = [
     path('', include(userRouter.urls)),
-    path('register', RegistrationAPIView.as_view()),
-    path('login', LoginAPIView.as_view()),
+    path('/register', RegistrationAPIView.as_view()),
+    path('/login', LoginAPIView.as_view()),
 ]
