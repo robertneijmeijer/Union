7c509c8f2b3360a20ab757bf5a8985e81efeaada
Koen Lippe <koen.lippe@quicknet.nl>
2021-04-14 09:10:52 +0200

    60 - Removed comment and reference

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,9 +1,5 @@
 import ApiBase from "@/api/api-base";
 
-// TODO -> Extend from ApiBase class so that every function in this class can call
-// TODO ApiBase.request({options}) so that f.e. headers can be set in the api base class
-// TODO -> can also be used to set the base URL
-
 export default class RegisterApi extends ApiBase {
 
     public static register = async (username: string, email: string, password: string): Promise<any> => {
--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -3,7 +3,7 @@ import ApiBase from "@/api/api-base";
 export default class UserApi extends ApiBase {
 
     public static getUsers = (): Promise<any> => {
-        return ApiBase.requestGetAll("users")
+        return UserApi.requestGetAll("users")
             .then(response => response)
             .catch(error => error)
     }
