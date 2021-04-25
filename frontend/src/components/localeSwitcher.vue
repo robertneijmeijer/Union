73a520f72921ec657a97400652f7ea56c4bf02bd
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2021-04-25 17:01:19 +0200

    Switching languages danamically

--- frontend/src/components/localeSwitcher.vue
+++ frontend/src/components/localeSwitcher.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
     <select class="locale-switcher" v-model="$i18n.locale">
-      <option value="en">English</option>
-      <option value="nl">Nederlands</option>
+      <option :value="locale.code" v-for="locale in locales" :key="locale.code">
+        {{ locale.name }}
+      </option>
     </select>
   </div>
 </template>
 
 <script>
+import {getSupportedLocales} from "../i18n/supported-locales";
+
 export default {
   name: "localeSwitcher",
-};
+  data() {
+    return {
+      locales: getSupportedLocales()
+    };
+  },
+}
+;
 </script>
 
 <style lang="scss" scoped>
--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -2,7 +2,7 @@ import { createI18n, LocaleMessages, VueMessageType } from "vue-i18n";
 
 function loadLocaleMessages(): LocaleMessages<VueMessageType> {
   const locales = require.context(
-    "./locales",
+    "../locales",
     true,
     /[A-Za-z0-9-_,\s]+\.json$/i
   );
