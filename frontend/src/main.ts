18b147718b72933a133730f1efa3d08a87b3550c
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2021-04-28 12:10:41 +0200

    UN 76/77 Switching languages danamically and set local language as default (#53)
    * Switching languages danamically
    
    * Setting default language

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { store } from "@/store/store";
 import router from "@/router";
-import i18n from "./i18n";
+import i18n from "./i18n/i18n";
 
 const app = createApp(App).use(i18n);
 
