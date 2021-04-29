69654c12abf4dfb9307b9db07418c3bbf70f34e1
Yan Alexandre <yan.lanna.alexandre@hva.nl>
2021-04-29 12:12:21 +0200

    data json formatted right, bad css practices removed

--- frontend/src/api/union.ts
+++ frontend/src/api/union.ts
@@ -2,7 +2,7 @@ import ApiBase from "@/api/api-base";
 
 export default class UnionApi extends ApiBase {
   public static postUnion = (data: any): Promise<any> => {
-    return UnionApi.requestPost("unions", { data: data }).then(
+    return UnionApi.requestPost("unions", { data }).then(
       response => response
     );
   };
