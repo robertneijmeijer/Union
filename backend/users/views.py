7bbffec5222c6d5679aa841485694425ff9201bd
Koen Lippe <koen.lippe@quicknet.nl>
2021-05-04 01:56:44 +0200

    Fix test

--- backend/users/views.py
+++ backend/users/views.py
@@ -22,8 +22,8 @@ class RegistrationAPIView(APIView):
         try:
             user = request.data.get('user')
             if user is None:
-                raise Exception
-        except KeyError:
+                raise ValueError
+        except ValueError:
             return Response({'errors': "Missing user key or empty body "}, status=status.HTTP_400_BAD_REQUEST)
 
         if User.objects.filter(username__iexact=user["username"]):
