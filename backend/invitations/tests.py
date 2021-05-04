786939197bd4c1d3f702da85fe33873210c637f4
Koen Lippe <koen.lippe@quicknet.nl>
2021-05-04 19:18:16 +0200

    80 - Improved tests

--- backend/invitations/tests.py
+++ backend/invitations/tests.py
@@ -34,24 +34,18 @@ class InvitationTests(APITestCase):
         self.union_members_cant_invite.union_users.add(self.teun)
 
     def test_create_invitation_endpoint(self):
-        invitation_data = {
-            "union_id": self.union.union_id
-        }
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.koen.token)
-        req = self.client.post('/unions/invite', invitation_data, format='json')
+        res, res_body = self.perform_request(self.koen, self.union.union_id)
 
         created_invite: Invitation = Invitation.objects.first()
-        content_unicode = req.content.decode('utf-8')
-        response_body = json.loads(content_unicode)
 
         # Invitation creation
         self.assertEqual(len(Invitation.objects.all()), 1)
         self.assertEqual(created_invite.invite_creator.user_id, self.koen.user_id)
         self.assertEqual(created_invite.union.union_id, self.union.union_id)
         # Response
-        self.assertEqual(req.status_code, status.HTTP_201_CREATED)
-        self.assertTrue("invite_token" in response_body)
-        self.assertEqual(response_body["invite_token"], created_invite.token)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertTrue("invite_token" in res_body)
+        self.assertEqual(res_body["invite_token"], created_invite.token)
 
     def test_create_with_invalid_union(self):
         res, res_body = self.perform_request(self.koen, 9999)
