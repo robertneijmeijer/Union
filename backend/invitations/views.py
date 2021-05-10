4e9576cd7365107a027841df0a2a2cb3e0c0d4e5
Koen Lippe <koen.lippe@quicknet.nl>
2021-05-10 13:34:39 +0200

    72 - API View v1

--- backend/invitations/views.py
+++ backend/invitations/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from authentication.backends import JWTAuthentication
+from invitations.models import Invitation
 from invitations.serializers import InvitationSerializer
 from project import settings
 
@@ -49,3 +52,38 @@ class InvitationsAPIView(APIView):
         serialized_data = serializer.data
 
         return Response(serialized_data, status=status.HTTP_201_CREATED)
+
+
+class InvitationsAcceptAPIView(APIView):
+    serializer_class = InvitationSerializer  # TODO: Needed?
+    permission_classes = [IsAuthenticated]
+
+    # TODO: Replace this complete new view with a function in the other view? Lets find out if thats possible
+    # TODO: This is the easiest for now
+
+    def post(self, request):
+        try:
+            parsed_token = request.GET['invite_token']
+            if parsed_token == "":
+                raise ValueError
+        except (MultiValueDictKeyError, ValueError):
+            return Response({"invite_token query params is missing/incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+
+        invite_token = request.GET['invite_token']
+        database_invitation: Invitation = Invitation.objects.filter(token=invite_token).first()
+
+        if database_invitation is None:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+        if not database_invitation.can_be_accepted():
+            return Response({"This invite has already been accepted"},
+                            status=status.HTTP_400_BAD_REQUEST)  # TODO: Check status for this
+
+        # TODO: Check for archived union if we want to implement it
+
+        user, token = JWTAuthentication.authenticate_credentials_from_request_header(request)
+
+        # Add user to unions
+        database_invitation.union.union_users.add(user)
+
+        return Response({}, status=status.HTTP_202_ACCEPTED)
--- backend/unions/urls.py
+++ backend/unions/urls.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from django.urls import include, path
 
-from invitations.views import InvitationsAPIView
+from invitations.views import InvitationsAPIView, InvitationsAcceptAPIView
 from unions.views import UnionViewSet
 
 unionRouter = routers.DefaultRouter()
@@ -10,4 +10,5 @@ unionRouter.register(r'', UnionViewSet)
 urlpatterns = [
     path('', include(unionRouter.urls)),
     path('/invite', InvitationsAPIView.as_view()),
+    path('/invite/accept', InvitationsAcceptAPIView.as_view()),
 ]
