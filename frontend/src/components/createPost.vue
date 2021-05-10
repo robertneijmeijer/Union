ddfbb89761394453a623f18e114a4f003f9e671a
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2021-05-10 11:42:44 +0200

    Created component

---
+++ frontend/src/components/createPost.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="create-card">
+    <p>Create post</p>
+    <div>
+      <p>Title</p>
+      <input
+          type="text"
+          name="title"
+          class="form-control input"
+          v-bind:placeholder="$t('register.email')"
+          v-model="title"/>
+    </div>
+    <div>
+      <p>Content</p>
+      <input
+          type="text"
+          name="content"
+          class="form-control input"
+          v-bind:placeholder="$t('register.email')"
+          v-model="content"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "createPostComponent",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/theme";
+
+.create-card {
+
+}
+
+</style>
