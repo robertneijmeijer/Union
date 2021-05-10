9f0e220de728b362f2c82f463f06d491108d48c8
Teun Stout <60847859+teunstout@users.noreply.github.com>
2021-05-10 11:41:33 +0200

    UN-88 Interceptor (#63)

---
+++ frontend/src/api/interceptor.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosRequestConfig } from "axios";
+import Cookie from "js-cookie";
+
+axios.interceptors.request.use(
+  function(config: AxiosRequestConfig) {
+    if (
+      !config.url ||
+      config.url?.indexOf("register") > -1 ||
+      config.url?.indexOf("login") > -1
+    )
+      return config;
+    config.headers = {
+      Authorization: `Token ${Cookie.get("Authorization")}`,
+    };
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
