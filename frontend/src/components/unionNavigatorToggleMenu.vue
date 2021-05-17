a4e7faac76c855b36d916bc28c06978b115224ce
Jverhagen11 <70578766+Jverhagen11@users.noreply.github.com>
2021-05-17 10:41:58 +0200

    Feature/un 81 route guards (#71)
    * route guards + logout button
    
    * formatting
    
    * global logout function
    
    * formatting index.ts
    
    Co-authored-by: joel <joel_verhagen_1997@hotmail.com>

--- frontend/src/components/unionNavigatorToggleMenu.vue
+++ frontend/src/components/unionNavigatorToggleMenu.vue
@@ -1,63 +1,72 @@
 <template>
-  <div class="menu">
-    <div class="item search">
-      <searchbar-component></searchbar-component>
+    <div class="menu">
+        <div class="item search">
+            <searchbar-component></searchbar-component>
+        </div>
+        <div class="item line">
+            <a href="">Account</a>
+        </div>
+        <div class="item line">
+            <div class="switcher">
+                <LocaleSwitcher class="switcher"/>
+            </div>
+        </div>
+        <div class="item line">
+            <a href="" v-on:click="logout()">Logout</a>
+        </div>
     </div>
-    <div class="item line">
-      <a href="">Account</a>
-    </div>
-    <div class="item line">
-      <div class="switcher">
-        <LocaleSwitcher class="switcher" />
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
-import SearchbarComponent from "@/components/searchbarComponent";
-import LocaleSwitcher from "@/components/localeSwitcher";
+  import SearchbarComponent from "@/components/searchbarComponent";
+  import LocaleSwitcher from "@/components/localeSwitcher";
+  import { logout } from "@/router";
 
-export default {
-  name: "union-toggle-menu",
-  components: { SearchbarComponent, LocaleSwitcher },
-};
+  export default {
+    name: "union-toggle-menu",
+    components: { SearchbarComponent, LocaleSwitcher },
+    methods: {
+      logout: function() {
+        logout();
+      },
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/theme";
+    @import "../assets/theme";
 
-.menu {
-  background-color: $primary-gray;
-}
+    .menu {
+        background-color: $primary-gray;
+    }
 
-.item {
-  padding: 10px 5px 10px 5px;
-}
+    .item {
+        padding: 10px 5px 10px 5px;
+    }
 
-.search {
-  margin: 0 10px 0 10px;
-}
+    .search {
+        margin: 0 10px 0 10px;
+    }
 
-a {
-  color: white;
-  text-decoration: none;
-  margin: 0 10px 0 10px;
-}
+    a {
+        color: white;
+        text-decoration: none;
+        margin: 0 10px 0 10px;
+    }
 
-.line {
-  border-bottom: 1px solid rgb(238, 235, 227);
-  opacity: 60%;
+    .line {
+        border-bottom: 1px solid rgb(238, 235, 227);
+        opacity: 60%;
 
-  &:hover {
-    background-color: black;
-    color: white;
-  }
-}
+        &:hover {
+            background-color: black;
+            color: white;
+        }
+    }
 
-.switcher {
-  color: white;
-  text-decoration: none;
-  margin: 0 10px 0 5px;
-}
+    .switcher {
+        color: white;
+        text-decoration: none;
+        margin: 0 10px 0 5px;
+    }
 </style>
