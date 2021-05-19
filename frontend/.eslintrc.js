579827aa2e3df31ae9a5cdc199d64b3860c13466
teunstout <teunstout@hotmail.com>
2021-05-19 14:16:25 +0200

    update migrations

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -16,6 +16,5 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
   },
 };
