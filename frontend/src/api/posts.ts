0716612b3f1d268cc2cd55d5f0ef030dbc962040
teunstout <teunstout@hotmail.com>
2021-05-19 14:18:02 +0200

    update request

--- frontend/src/api/posts.ts
+++ frontend/src/api/posts.ts
@@ -1,16 +1,11 @@
 import ApiBase from "@/api/api-base";
 import { AxiosResponse } from "axios";
-
-export type UnionType = {
-  name: string;
-  description: string;
-  members_can_invite: boolean;
-  icon?: string;
-  banner?: string;
-};
+import { UnionType } from "./union";
 
 export default class UnionApi extends ApiBase {
-  public static getAllPosts = (unionNamen: string): Promise<AxiosResponse<UnionType>> => {
-    return UnionApi.requestGet<UnionType>("posts", unionNamen);
+  public static getAllPosts = (
+    unionNames: string
+  ): Promise<AxiosResponse<UnionType>> => {
+    return UnionApi.requestGet<UnionType>("posts", unionNames);
   };
 }
