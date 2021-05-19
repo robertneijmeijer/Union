429b34cb0770b361d2c28f4ba2b9a5911f3de406
Yan Alexandre <yan.lanna.alexandre@hva.nl>
2021-05-19 15:36:21 +0200

    search icon added

--- frontend/src/components/searchbarComponent.vue
+++ frontend/src/components/searchbarComponent.vue
@@ -1,5 +1,7 @@
 <template>
   <form>
+    <svg class="search-icon" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15.59,13.91l2.78,2.69a1.25,1.25,0,1,1-1.74,1.8l-2.82-2.73a8,8,0,1,1,1.78-1.76ZM14.64,9.2A5.45,5.45,0,1,0,9.2,14.64,5.45,5.45,0,0,0,14.64,9.2Z" fill="#818384"></path>
+    </svg>
     <input
       class="form-control search"
       type="search"
@@ -23,5 +25,14 @@ export default {
   color: white;
   max-width: 100%;
   display: inline;
+  padding-left: 5%;
+}
+
+.search-icon {
+  width: 18px;
+  height: 18px;
+  position: absolute;
+  top: 19px;
+  margin-left: 10px;
 }
 </style>
