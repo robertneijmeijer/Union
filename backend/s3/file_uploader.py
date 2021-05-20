9c41c14efcce4d1a0629dfb7d7f8eddfc1f621d8
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2021-05-20 14:15:03 +0200

    Update file_uploader.py

--- backend/s3/file_uploader.py
+++ backend/s3/file_uploader.py
@@ -1,10 +1,34 @@
+from django.core import files
 from minio import Minio
 from minio.error import S3Error
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django import forms
 import os, sys, logging
 
 MINIO_ADDRESS= os.environ.get('MINIO_ADDRESS')
 MINIO_ACCESS_KEY= os.environ.get('MINIO_ACCESS_KEY')
 MINIO_SECRET_KEY= os.environ.get('MINIO_SECRET_KEY')
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
+
+def SaveFile(request):
+    model_file = request.FILES['banner']
+
+    path = default_storage.save('./tempStorage', ContentFile(b))
+
+
+
+
+
+
+    logging.warning("CONTENT")
+    logging.warning(model_file.content_type)
+    with open('./tempStorage', 'wb+') as destination:
+        for chunk in model_file.chunks():
+            destination.write(chunk)
+
 
 def file_uploader(name,file):
     if not name or not file:
@@ -26,13 +50,12 @@ def file_uploader(name,file):
     else:
         print("Bucket already exists")
 
-    try:
-        result = client.fput_object(
-            bucketName,name,file
-        )
-        print(result)
-    except:
-        print("error while putting")
+    logging.warning(file)
+    url = client.fput_object(
+        bucketName,name,'https://cdn.searchenginejournal.com/wp-content/uploads/2020/05/reverse-image-search-your-complete-guide-5ef5f5435f949-760x400.png'
+    )
+    logging.warning("Result of put", url)
+    return url
 
 
 if __name__ == "__file_uploader__":
