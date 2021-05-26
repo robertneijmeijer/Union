d6dcde1d57e29177e55dd525242839a4014452e0
Koen Lippe <koen.lippe@quicknet.nl>
2021-05-26 10:58:04 +0200

    52 - Regsiter form handling (#45)
    * 52 - Mapped input to state
    
    * 52 - Removed obsolete form actions
    
    * 52 -  Backend user validation
    
    * 52 - Watch input values and do validate request
    
    * 52 - Remove logs and check for double password
    
    * 52 - Eslint
    
    * 52 - Translate error messages and put them in component
    
    * Eslint fix
    
    * Unset error after retype
    
    * Set all form errors
    
    * 52 - Removed double styling and changed to min-height
    
    * 52 - Unset password error on submit
    
    * 52 - Eslint
    
    * 52 - Formatting fix
    
    * Added validation
    
    * More regex
    
    * Added comment and added validation to component
    
    * Update
    
    * Fixed export
    
    * Fixed error
    
    * Added password validation
    
    * PR review
    
    * More changes
    
    * Renaming
    
    * Formatting fix?
    
    * 52 - Fix wrong username in register request
    
    * 52 - Removed comment and eslint fix
    
    * Added validate to interceptor
    
    * Real time checking of password.
    
    * Disabled submit button
    
    * Removed log
    
    Co-authored-by: Robert Neijmeijer <robert98neijmeijer@gmail.com>

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,7 +1,6 @@
 import { CommitOptions, createStore, DispatchOptions, Store } from "vuex";
 import { user, UserModuleStateInterface } from "@/store/modules/user";
 import { ActionsInterface as userActions } from "@/actions/user";
-import { ActionsInterface as formActions } from "@/actions/form";
 import { MutationsInterface as userMutations } from "@/mutations/user";
 import { MutationsInterface as formMutations } from "@/mutations/form";
 import { form, FormModuleStateInterface } from "@/store/modules/form";
@@ -13,7 +12,7 @@ export interface RootState {
   form: FormModuleStateInterface;
 }
 export type MutationTypes = userMutations & formMutations;
-export type ActionTypes = userActions & formActions;
+export type ActionTypes = userActions;
 
 // Override commit and dispatch to only accept our own typings
 export interface StoreInterface
