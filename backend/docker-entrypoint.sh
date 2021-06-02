d00ea6fe752d2145aecc946c9c626225f2199cdc
teunstout <teunstout@hotmail.com>
2021-06-02 18:09:22 +0200

    update shell scrippje

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 echo "Apply database migrations"
 python manage.py migrate
 echo "Running server" python
-manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
