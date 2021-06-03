bf3c800ebf2f0c084ecaee6dad44e74baebfdd9d
Jverhagen11 <70578766+Jverhagen11@users.noreply.github.com>
2021-06-03 10:41:09 +0200

    Feature/un 106 get union overview (#97)
    * return list of union user
    
    * returns joined union data
    
    * linked backend and frontend
    
    * import
    
    * linting
    
    * linting
    
    Co-authored-by: joel <joel_verhagen_1997@hotmail.com>

--- backend/unions/urls.py
+++ backend/unions/urls.py
@@ -2,8 +2,8 @@ from rest_framework import routers
 from django.urls import include, path
 
 from invitations.views import InvitationsAPIView, InvitationsAcceptAPIView
+from unions.views import UnionViewSet, UnionOverviewAPIView
 from unionImages.views import UnionImagesViewSet
-from unions.views import UnionViewSet
 
 unionRouter = routers.DefaultRouter()
 unionRouter.register(r'', UnionViewSet)
@@ -13,4 +13,5 @@ urlpatterns = [
     path('', include(unionRouter.urls)),
     path('/invite', InvitationsAPIView.as_view()),
     path('/invite/accept', InvitationsAcceptAPIView.as_view()),
+    path('/overview', UnionOverviewAPIView.as_view())
 ]
--- frontend/src/views/unionOverview.vue
+++ frontend/src/views/unionOverview.vue
@@ -1,17 +1,29 @@
 <template>
-    <UnionOverviewNavigator></UnionOverviewNavigator>
-    <UnionDashboard></UnionDashboard>
+  <union-overview-navigator></union-overview-navigator>
+  <union-dashboard :unions="unionState"></union-dashboard>
 </template>
 
 <script>
-  import UnionOverviewNavigator from "@/components/unionOverviewNavigator";
-  import UnionDashboard from "@/components/unionDashboard";
-  export default {
-    name: "unionOverview",
-    components: { UnionDashboard, UnionOverviewNavigator },
-  };
+import UnionOverviewNavigator from "../components/unionOverviewNavigator.vue";
+
+// eslint-disable-next-line
+import { defineComponent } from "vue";
+// eslint-disable-next-line
+import { ActionTypes } from "@/actions/union";
+import UnionDashboard from "../components/unionDashboard.vue";
+
+export default defineComponent({
+  name: "unionOverview",
+  components: { UnionDashboard, UnionOverviewNavigator },
+  created() {
+    this.$store.dispatch(ActionTypes.UNION_ACTION_FETCH_OVERVIEW);
+  },
+  computed: {
+    unionState() {
+      return this.$store.state.union.unions;
+    },
+  },
+});
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
