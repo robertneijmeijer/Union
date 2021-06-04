37ed610e9c346808e20f69bb0c8ad3c1d4f2871a
joel <joel_verhagen_1997@hotmail.com>
2021-06-04 13:23:18 +0200

    druk maar lekker niet op de terugknop

--- frontend/src/components/unionListItem.vue
+++ frontend/src/components/unionListItem.vue
@@ -1,114 +1,114 @@
 <template>
-  <a v-on:click="onClickUnion()" class="table-union-content">
-    <div class="banner">
-      <img
-        class="image"
-        :src="require(`../assets/img/${unionsArray.union.banner}`)"
-        alt="responsive image"
-      />
-    </div>
-    <div class="union">
-      <h2 class="name">{{ unionsArray.union.name }}</h2>
-    </div>
-    <div id="icon" class="icon-content">
-      <img
-        class="image-logo"
-        :src="require(`../assets/img/${unionsArray.union.icon}`)"
-      />
-    </div>
-  </a>
+    <a v-on:click="onClickUnion()" class="table-union-content">
+        <div class="banner">
+            <img
+                    class="image"
+                    :src="require(`../assets/img/${unionsArray.union.banner}`)"
+                    alt="responsive image"
+            />
+        </div>
+        <div class="union">
+            <h2 class="name">{{ unionsArray.union.name }}</h2>
+        </div>
+        <div id="icon" class="icon-content">
+            <img
+                    class="image-logo"
+                    :src="require(`../assets/img/${unionsArray.union.icon}`)"
+            />
+        </div>
+    </a>
 </template>
 
 <script>
   import router, { toOverview } from "@/router";
 
-export default {
-  name: "unionListItem",
-  props: {
-    unionsArray: Object,
-  },
-  methods: {
-    onClickUnion() {
-      let name
-      if(this.unionsArray.union && this.unionsArray.union.name) {
-        name = this.unionsArray.union.name
-        router.push('/union/'+name);
-      } else {
-          toOverview()
-      }
+  export default {
+    name: "unionListItem",
+    props: {
+      unionsArray: Object,
     },
-  }
-};
+    methods: {
+      onClickUnion() {
+        let name;
+        if (this.unionsArray.union && this.unionsArray.union.name) {
+          name = this.unionsArray.union.name;
+          router.push("/union/" + name);
+        } else {
+          toOverview();
+        }
+      },
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/theme";
+    @import "../assets/theme";
 
-@media only screen and (max-width: 1400px) {
-  #icon {
-    left: 30px;
-  }
-}
+    @media only screen and (max-width: 1400px) {
+        #icon {
+            left: 30px;
+        }
+    }
 
-@media only screen and (max-width: 1000px) {
-  #icon {
-    left: 40px;
-    top: 110px;
-    max-height: 70px;
-    max-width: 70px;
-  }
-}
+    @media only screen and (max-width: 1000px) {
+        #icon {
+            left: 40px;
+            top: 110px;
+            max-height: 70px;
+            max-width: 70px;
+        }
+    }
 
-.table-union-content {
-  height: 250px;
-  position: relative;
-  padding: 6px;
-  border-radius: $borderRadius;
-  opacity: 80%;
+    .table-union-content {
+        height: 250px;
+        position: relative;
+        padding: 6px;
+        border-radius: $borderRadius;
+        opacity: 80%;
 
-  &:focus,
-  &:active,
-  &:hover {
-    background-color: $primary-gray;
-    opacity: 100%;
-    text-decoration: none;
-  }
-}
+        &:focus,
+        &:active,
+        &:hover {
+            background-color: $primary-gray;
+            opacity: 100%;
+            text-decoration: none;
+        }
+    }
 
-.banner {
-  height: 60%;
-}
+    .banner {
+        height: 60%;
+    }
 
-.union {
-  padding-top: 25px;
-  height: 40%;
-  background-color: $primary-black;
-  border-radius: 0 0 33px 33px;
-}
+    .union {
+        padding-top: 25px;
+        height: 40%;
+        background-color: $primary-black;
+        border-radius: 0 0 33px 33px;
+    }
 
-.image {
-  width: 100%;
-  height: 100%;
-  border-radius: 33px 33px 0 0;
-}
+    .image {
+        width: 100%;
+        height: 100%;
+        border-radius: 33px 33px 0 0;
+    }
 
-.name {
-  font-size: 22px;
-  color: white;
-  text-align: center;
-  padding: 8px;
-}
+    .name {
+        font-size: 22px;
+        color: white;
+        text-align: center;
+        padding: 8px;
+    }
 
-.image-logo {
-  width: 100%;
-  height: 100%;
-}
+    .image-logo {
+        width: 100%;
+        height: 100%;
+    }
 
-.icon-content {
-  max-height: 75px;
-  max-width: 75px;
-  position: absolute;
-  top: 100px;
-  left: 45px;
-}
+    .icon-content {
+        max-height: 75px;
+        max-width: 75px;
+        position: absolute;
+        top: 100px;
+        left: 45px;
+    }
 </style>
