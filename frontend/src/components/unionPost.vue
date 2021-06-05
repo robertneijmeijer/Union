15cbb893f7bf6e19979bb27780a1b90b21a0f4e0
joel <joel_verhagen_1997@hotmail.com>
2021-06-05 15:55:05 +0200

    to specific post

--- frontend/src/components/unionPost.vue
+++ frontend/src/components/unionPost.vue
@@ -38,8 +38,8 @@
       </button>
     </div>
 
-    <div class="table-post-content">
-      <h4>{{ post.title }}</h4>
+    <div  class="table-post-content">
+      <h4> <a href="" v-on:click="toPost()">{{ post.title }} </a></h4>
       <p class="table-post-text">{{ post.message }}</p>
       <div class="table-comments-amount">
         <a
@@ -48,7 +48,7 @@
           v-if="post.number_of_comments && post.number_of_comments > 0"
           >Show comments ({{ number_of_comments }})</a
         >
-        <a href="" v-on:click="toComments" v-else>Be the first to comment</a>
+        <a href="" v-on:click="toPost" v-else>Be the first to comment</a>
       </div>
     </div>
   </div>
@@ -57,6 +57,7 @@
 <script>
 import { ActionTypes } from "../actions/union";
 import PostApi, { VoteENUM } from "../api/posts";
+import router from "@/router"
 
 export default {
   name: "union-post-component",
@@ -68,6 +69,11 @@ export default {
     this.vote(this.post.user_vote, true);
   },
   methods: {
+    toPost() {
+      let post_id = this.post.post_id
+      router.push("/post/"+post_id)
+    },
+
     upvote() {
       this.vote(VoteENUM.UPVOTE, false);
     },
