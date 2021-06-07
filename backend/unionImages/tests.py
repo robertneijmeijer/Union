6681ef1ff244aee2c76e21d24638e1a61a49e46f
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2021-06-07 16:35:37 +0200

    Update tests.py

--- backend/unionImages/tests.py
+++ backend/unionImages/tests.py
@@ -6,10 +6,11 @@ from unionImages.models import UnionImages
 
 client = APIClient()
 
+
 class UnionImagesTest(APITestCase):
     def setUp(self):
         return super().setUp()
 
     def test_create(self):
         with open('../assets/defaultBanner.png') as banner:
-            self.assertTrue( client.post('/unions/images/', {'name': 'test', 'attachment': banner}))
+            self.assertTrue(client.post('/unions/images/', {'name': 'test', 'attachment': banner}))
