d59ce27fc826ee369929bdbaee4e4813f819e0a3
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2021-06-07 17:59:14 +0200

    Update tests.py

--- backend/unionImages/tests.py
+++ backend/unionImages/tests.py
@@ -16,7 +16,13 @@ class UnionImagesTest(APITestCase):
             '/unions/images/', {'union_id': 'test', 'banner': '', 'icon': ''})
         self.assertEqual(result.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_fail(self):
+    def test_no_images(self):
         self.assertRaises(Exception, client.post(
             '/unions/images/', {'union_id': 'test', 'banner': '', 'icon': ''}),
             '123')
+
+    def test_no_images(self):
+        result = client.post('/unions/images/', 
+        {'union_id': ['adsfadsgasg'], 'banner': [''], 'icon': ['']}, 
+        content_type='multipart/form-data;·boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW')
+        self.assertTrue(result)
